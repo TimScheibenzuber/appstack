1abfbceb7284458b9bb2767ed27b65104182e853
Tim Scheibenzuber <tim.scheibenzuber@web.de>
2025-09-11 13:07:49 +0200

    Created a cs file and wrote some code

---
+++ server/HelloCSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿// using = Bibliotheken einbinden (vergleichbar mit "import" in Python)
+using System;
+
+// Das sind "Top-Level Statements": Einstiegspunkt ohne extra Main-Methode.
+Console.WriteLine("Hallo Tim!");
+// Variablen sind typisiert: int = ganze Zahl
+int a = 3;
+int b = 4;
+int summe = a + b;
+Console.WriteLine($"Summe: {summe}");
+// $"..." = Stringinterpolation (Variablen im Text)
